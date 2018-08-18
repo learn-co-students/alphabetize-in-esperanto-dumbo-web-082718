4f7b76ccb5d302cab2dc33a0db30a72e1e12802a
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2018-08-18 18:44:50 -0400

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  sorted_array = arr.sort_by do |word|
+    word.split("").collect do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
+  return sorted_array
 end
